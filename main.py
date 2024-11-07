c8c0deb0c779d355de28a4b8c3706520bbc37484
bzelaznicki <bartosz@zelaznicki.com>
2024-11-07 16:52:05 +0100

    the triangle appears

--- main.py
+++ main.py
@@ -3,19 +3,30 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+    game_clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    starting_x = SCREEN_WIDTH / 2
+    starting_y = SCREEN_HEIGHT / 2
+    player = Player(starting_x, starting_y)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(0)
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
+        
+        
 
     
 
